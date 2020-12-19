1209b6dc05e2ad59c16eb75e6dff44d32f129104
ajundi <70916139+ajundi@users.noreply.github.com>
2020-12-19 10:37:19 -0500

    Add project files.

---
+++ OpenTapVisa/OperatingSystem.cs
@@ -0,0 +1,115 @@
+//            Copyright Keysight Technologies 2012-2019
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, you can obtain one at http://mozilla.org/MPL/2.0/.
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace OpenTap
+{
+    /// <summary> Detects which operating system is used. </summary>
+    class OperatingSystem
+    {
+        public static readonly OperatingSystem Windows = new OperatingSystem(nameof(Windows));
+        public static readonly OperatingSystem Linux = new OperatingSystem(nameof(Linux));
+        public static readonly OperatingSystem Unsupported = new OperatingSystem(nameof(Unsupported));
+        public override string ToString() => Name;
+        public string Name { get; }
+        OperatingSystem(string name)
+        {
+            Name = name;
+        }
+
+        static OperatingSystem getCurrent()
+        {
+
+            if (Path.DirectorySeparatorChar == '\\')
+            {
+                return OperatingSystem.Windows;
+            }
+            else
+            {
+                if (Directory.Exists("/proc/"))
+                {
+                    return OperatingSystem.Linux;
+                }
+            }
+            return OperatingSystem.Unsupported;
+        }
+
+        static OperatingSystem current;
+        public static OperatingSystem Current
+        {
+            get
+            {
+                if (current == null)
+                {
+                    current = getCurrent();
+                }
+                return current;
+            }
+        }
+    }
+
+    /// <summary> Detection of the specific linux variant. </summary>
+    class LinuxVariant
+    {
+        public string Name { get; }
+        public static readonly LinuxVariant Debian = new LinuxVariant("debian");
+        public static readonly LinuxVariant Ubuntu = new LinuxVariant("ubuntu");
+        public static readonly LinuxVariant RedHat = new LinuxVariant("redhat");
+        public static readonly LinuxVariant Unknown = new LinuxVariant("linux-x64");
+
+        static LinuxVariant()
+        {
+            if (OperatingSystem.Current == OperatingSystem.Linux)
+            {
+                var os_release_file = new FileInfo("/etc/os-release");
+                if (os_release_file.Exists)
+                {
+                    Current = Unknown;
+                    using (var str = new StreamReader(os_release_file.OpenRead()))
+                    {
+                        
+                        string line;
+                        while ((line = str.ReadLine()) != null)
+                        {
+                            if(line.Contains("NAME=\"Debian GNU/Linux\""))
+                            {
+                                Current = Debian;
+                                return;
+                            }
+                            if(line.Contains("NAME=\"Ubuntu\""))
+                            {
+                                Current = Ubuntu;
+                                return;
+                            }
+                            
+                            if (line.Contains("NAME=\"Red Hat"))
+                            {
+                                Current = RedHat;
+                                return;
+                            }
+
+                            if (line.Contains("NAME=\"CentOS Linux\""))
+                            {
+                                // pretend CentOS is Red Hat for simplicity.
+                                Current = RedHat;
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        
+        public static LinuxVariant Current { get; }
+
+        public LinuxVariant(string name) => Name = name;
+
+    }
+    
+}
---
+++ VNA/Program.fs
@@ -0,0 +1,76 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open XPlot.Plotly   
+open OpenTapVisa
+
+
+type vna (address)=
+    let mutable sesn=0
+    let mutable vi=1
+    let mutable retCount=0
+    let mutable Readbuffer=""
+    let mutable status=Visa.viOpenDefaultRM &sesn
+    do
+        status <- Visa.viOpen(sesn,address,0,0,&vi) 
+    member this.Status with get() =status 
+    member this.write (command:string)= status <-Visa.IOWrite(vi,command,&retCount)
+    member this.query (command)=status <- Visa.IOQuery(vi,"*opc?",&Readbuffer)
+
+[<EntryPoint>]
+
+let main argv =
+    printfn "Hello World from F#!"
+    // Learn more about F# at http://fsharp.org
+    let mutable sesn1=0
+    let mutable status= Visa.viOpenDefaultRM &sesn1
+    let mutable vi=1
+    let mutable retCount=0
+    let mutable Readbuffer=""
+    printfn "Hello World from F#!"
+    let VNA="TCPIP0::localhost::hislip0::INSTR"
+    
+    let GetIden address=
+        status <- Visa.viOpen(sesn1,address,0,0,&vi) 
+        status <- Visa.IOQuery(vi,"*IDN?",&Readbuffer)
+        if status<>0 then  printfn  "Error writing Command"
+        else    status <- Visa.viClose(vi)
+                printfn "%s" Readbuffer
+    GetIden VNA
+    
+    let EvenIdx values= let End=(Array.length values)-1
+                        [|0..2..End|] |>Array.map (Array.get values) 
+    
+    let OddIdx values = let End=(Array.length values)-1
+                        [|1..2..End|] |>Array.map (Array.get values) 
+    let GetTrace snn address=
+        let vna=vna(address)
+        status <- Visa.IOWrite(vi,"SENSe1:SWEep:POINts 2000",&retCount)
+        status <- Visa.viOpen(sesn1,address,0,0,&vi) 
+        status <- Visa.IOWrite(vi,"CALC1:PARameter1:DEF "+snn,&retCount)
+        vna.write("CALC1:PARameter1:DEF "+snn)
+        vna.write("TRIG:SOUR BUS\n")
+        vna.write("TRIG:SING\n")
+        vna.query("*OPC?\n")
+        //status <- Visa.IOWrite(vi,"SENSe1:SWEep:POINts 2000",&retCount)
+        status <- Visa.IOQuery(vi,"*opc?",&Readbuffer)
+        //Thread.Sleep 0
+        status <- Visa.IOQuery(vi,"SENSe1:FREQuency:Data?",&Readbuffer)
+        if status<>0 then  printfn  "Error writing Command"
+        let F=Readbuffer.Split ','
+        status <- Visa.IOQuery(vi,"CALC1:trac1:DATA:FDATa?",&Readbuffer)
+        if status<>0 then  printfn  "Error writing Command"
+        let Y=Readbuffer.Split ',' 
+        let Y1=EvenIdx Y
+        let Y2=OddIdx Y
+        [F;Y1;Y2] |>List.map (Array.map float) 
+    
+    let layout =  Layout( showlegend = false ,smith=true, plot_bgcolor = "rgb(255,255,255)")
+    let S21=(GetTrace "S21" VNA) 
+    let S11=(GetTrace "S11" VNA) 
+    
+    [Seq.zip S21.[0] S21.[1] ; Seq.zip S11.[0] S11.[1]]
+    |> Chart.Line
+    |> Chart.WithTitle "S21"
+    |> Chart.WithLayout layout
+    |> Chart.Show
+    1
